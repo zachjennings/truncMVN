6b52a2e1cd41c62e35c3dc32d37e8ff20a6e59f2
Zach Jennings <zach.jennings@gmail.com>
2016-05-23 16:26:37 -0700

    added truncated MVN code

---
+++ truncMVN.py
@@ -0,0 +1,87 @@
+import numpy as np
+import scipy.stats as stats
+from scipy.stats import mvn
+
+class truncMVN(object):
+    """
+    Class to calculate PDF of a 2D truncated multivariate normal distribution.
+    
+    Uses method of Alan Genz to calculate MVN CDF, implemented in scipy.
+    
+    low_int_limit: lower limit for *integration* of the CDFs. This is different than the lower
+    limit of the bound we want to normalize! Setting to a value several SDs lower than mean should be fine.
+    
+    low: lower limit for the bound that we want to normalize over. Again, different from the
+    lower integration limit!
+    
+    high: upper limit for the bound that we want to normalize over
+    
+    calc_norm: If true, calculate normalization at initialization. 
+    """
+    def __init__(self, means=None,cov=None,low=None,high=None,low_int_limit=np.array([-10,10]),calc_norm=False):                    
+        self.low_int_limit = low_int_limit
+        self.low = low
+        self.high = high
+        
+        if calc_norm:
+            self.norm = self.normalize(means,cov,high,low)
+            
+    
+    def normalize(self,means=None,cov=None,low=None,high=None):
+        """
+        Calculate normalization term for the truncated MVN.
+        
+        Involves calculation of four CDF terms:
+        P(4) - P(3) - P(2) + P(1)
+        """
+        #calculate CDF of full region
+        cdf_4,i = mvn.mvnun(self.low_int_limit,high,means,cov)
+        
+        #calculate CDFs of outside regions
+        cdf_3,i = mvn.mvnun(self.low_int_limit,np.array([high[0],low[1]]),means,cov)
+        cdf_2,i = mvn.mvnun(self.low_int_limit,np.array([low[0],high[1]]),means,cov)
+        
+        #calculate CDF of lower-left corner region
+        cdf_1,i = mvn.mvnun(self.low_int_limit,low,means,cov)
+        
+        reg_prob = (cdf_4 - cdf_3 - cdf_2 + cdf_1)
+                
+        return 1./reg_prob
+            
+        
+    def logpdf(self,data,means=np.array([]),cov=np.array([]),re_norm=True):
+        """
+        Calculate the logpdf for a truncated MVN
+        
+        data = 2 x n array containing data to calc PDFs for.
+        means: 2 x 1 array value for MVN means
+        cov:  2x2 covariance matrix
+        
+        re_norm: if true, re-calculate normalization of the 
+        """
+        lpdf = stats.multivariate_normal.logpdf(data,mean=means,cov=cov)
+        
+        if re_norm:
+            norm = self.normalize(means,cov,self.low,self.high)
+        else:
+            norm = self.norm
+            
+        return np.log(norm) + lpdf
+        
+        
+    def pdf(self,data,means=np.array([]),cov=np.array([]),re_norm=True):
+        """
+        Calculate the pdf for a truncated MVN
+        
+        data = 2 x n array containing data to calc PDFs for.
+        means: 2 x 1 array value for MVN means
+        cov:  2x2 covariance matrix
+        """
+        pdf = stats.multivariate_normal.logpdf(data,mean=means,cov=cov)
+        
+        if re_norm:
+            norm = self.normalize(means,cov,self.low,self.high)
+        else:
+            norm = self.norm
+            
+        return norm * pdf
